4bfd05688502998dea811b6ecdedcafad507bad6
Shae Jacobs <shae.jacobs01@gmail.com>
2024-09-12 14:25:51 +1200

    added to the component to display data

--- client/components/CafeProfile.tsx
+++ client/components/CafeProfile.tsx
@@ -1,3 +1,28 @@
+import { useCafeById } from '@/hooks/useCafe'
+import { useParams } from 'react-router-dom'
+
 export default function CafeProfile() {
-  return <section className="section">Cafe Profile</section>
+  const params = useParams()
+  const id = params.cafeId
+  const { data: cafe, isError, error, isPending } = useCafeById(String(id))
+  if (isPending) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+  if (isError) {
+    return <p>Theres been an error getting cafe,{error.message}</p>
+  }
+  return (
+    <section className="section">
+      <img src={`${cafe.image}`} alt={`${cafe.name}`} />
+      <div>
+        <h1>{cafe.name}</h1>
+        <p>{cafe.suburb}</p>
+        <p>{cafe.streetAddress}</p>
+      </div>
+    </section>
+  )
 }
