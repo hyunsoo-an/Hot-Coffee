6ffef47307f90ada74ba6ad74ce08a37d9ba16a9
Shae Jacobs <shae.jacobs01@gmail.com>
2024-09-12 16:13:19 +1200

    added some tailwind and displaying more data

--- client/components/CafeProfile.tsx
+++ client/components/CafeProfile.tsx
@@ -1,5 +1,6 @@
 import { useCafeById } from '@/hooks/useCafe'
 import { useParams } from 'react-router-dom'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
 
 export default function CafeProfile() {
   const params = useParams()
@@ -17,11 +18,22 @@ export default function CafeProfile() {
   }
   return (
     <section className="section">
-      <img src={`${cafe.image}`} alt={`${cafe.name}`} />
+      <AspectRatio ratio={16 / 9} className="overflow-hidden">
+        <img
+          src="https://plus.unsplash.com/premium_photo-1663932464937-e677ddfc1d55?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt={`${cafe.name}`}
+          className="object-cover"
+        />
+      </AspectRatio>
       <div>
-        <h1>{cafe.name}</h1>
-        <p>{cafe.suburb}</p>
-        <p>{cafe.streetAddress}</p>
+        <h1 className="text-center text-xl font-bold">{cafe.name}</h1>
+        <p className="text-base text-xs">
+          {cafe.streetAddress}
+          <br />
+          {cafe.suburb}
+          <br />
+          {cafe.city}
+        </p>
       </div>
     </section>
   )
