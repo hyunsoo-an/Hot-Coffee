e0a7aacdcb322f06a9d0a8096d61975762f5d3f6
Hyunsoo An <hankjudolove@gmail.com>
2024-09-13 10:01:33 +1200

    working on styling

--- client/components/CafeListItem.tsx
+++ client/components/CafeListItem.tsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 export default function CafeListItem({ cafe }: { cafe: Cafe }) {
   return (
-    <div className="cafe-list-item">
-      <Link to={`/cafes/${cafe.id}`}>
-        <h3>{cafe.name}</h3>
-        <p>{cafe.suburb}</p>
-        <p>{cafe.avgRating}</p>
+    <div className="mb-4 rounded-lg shadow-md">
+      <Link to={`/cafes/${cafe.id}`} className="m-3 flex flex-col flex-wrap">
+        <h3 className="text-xl font-bold text-gray-800">{cafe.name}</h3>
+        <p className="text-gray-600">{cafe.suburb}</p>
+        <p className="text-2xl font-semibold text-gray-700">{cafe.avgRating}</p>
       </Link>
     </div>
   )
