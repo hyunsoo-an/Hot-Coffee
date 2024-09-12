c63e6f41a9bb1cf58f642030ea025678aca44ce4
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-09-12 18:06:18 +1200

    Use correct headings for cafe list

--- client/components/CafeListItem.tsx
+++ client/components/CafeListItem.tsx
@@ -7,7 +7,7 @@ export default function CafeListItem({ cafe }: { cafe: Cafe }) {
       to={`/cafes/${cafe.id}`}
       className="grid auto-rows-max grid-cols-[1fr_auto]"
     >
-      <h3 className="font-semibold">{cafe.name}</h3>
+      <h2 className="font-semibold">{cafe.name}</h2>
       <p className="text-sm text-muted-foreground">{cafe.suburb}</p>
       <p className="col-start-2 row-span-2 row-start-1 text-lg font-black">
         {cafe.avgRating}
