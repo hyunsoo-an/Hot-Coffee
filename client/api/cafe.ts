78409c1e87c8210f81cf72516ad3fd9eafb48fff
Hyunsoo An <hankjudolove@gmail.com>
2024-09-12 16:21:31 +1200

    uncommented apiPath

--- client/api/cafe.ts
+++ client/api/cafe.ts
@@ -1,14 +1,12 @@
 import request from 'superagent'
 import { Cafe } from '../../models/cafes'
 
-//const apiPath = '/api/v1/cafes'
+const apiPath = '/api/v1/cafes'
 
 export async function getCafeList(suburb?: string): Promise<Cafe[]> {
-  // let endpoint = ''
-  // endpoint = suburb ? `?suburb=${suburb}` : ''
+  let endpoint = ''
+  endpoint = suburb ? `?suburb=${suburb}` : ''
 
-  const result = await request.get('/api/v1/cafes')
+  const result = await request.get(apiPath + endpoint)
   return result.body as Cafe[]
-  // const result = await request.get(apiPath + endpoint)
-  // return result.body as Cafe[]
 }
