bb7a078518b7647385ac5795ae0fe32a1f2dfdba
Rowan Strang <rowanstrang@gmail.com>
2024-09-13 16:15:47 +1200

    ip address gets at run

--- client/api/getUserIP.ts
+++ client/api/getUserIP.ts
@@ -2,6 +2,7 @@ import request from 'superagent'
 export const getUserIP = async (): Promise<string> => {
   try {
     const res = await request.get('https://api.ipify.org/?format=json')
+    console.log(res.body)
     return res.body.ip
   } catch (error) {
     console.error('Error fetching IP address:', error)
