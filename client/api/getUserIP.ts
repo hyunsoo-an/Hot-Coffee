f2d80dfc2143627f21843990d52ff970a039c38a
Rowan Strang <rowanstrang@gmail.com>
2024-09-13 16:23:29 +1200

    removed console log from getUserIP

--- client/api/getUserIP.ts
+++ client/api/getUserIP.ts
@@ -2,7 +2,6 @@ import request from 'superagent'
 export const getUserIP = async (): Promise<string> => {
   try {
     const res = await request.get('https://api.ipify.org/?format=json')
-    console.log(res.body)
     return res.body.ip
   } catch (error) {
     console.error('Error fetching IP address:', error)
