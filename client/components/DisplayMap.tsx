8a55b0d846f3f5d2ddfe50e3cdee6860ef2ed710
Shae Jacobs <shae.jacobs01@gmail.com>
2024-09-13 16:03:15 +1200

    finsihed map component

--- client/components/DisplayMap.tsx
+++ client/components/DisplayMap.tsx
@@ -1,24 +1,51 @@
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import { GoogleMap, LoadScriptNext, Marker } from '@react-google-maps/api'
 import { API_HOST } from '../env'
+import { useCafeById } from '@/hooks/useCafe'
+import { useParams } from 'react-router-dom'
+
+const defaultCenter = {
+  lat: -41.296881158626114,
+  lng: 174.77414413921622,
+}
+
+const mapStyles = {
+  height: '500px',
+  width: '100%',
+}
+
 export default function DisplayMap() {
-  const mapStyles = {
-    height: '400px',
-    width: '100%',
+  const params = useParams()
+  const id = params.cafeId
+
+  const { data: cafe, isError, error, isPending } = useCafeById(String(id))
+  if (isPending) {
+    return (
+      <div>
+        <h1>Loading map....</h1>
+      </div>
+    )
+  }
+  if (isError) {
+    return <h1>Error Loading map {error.message}</h1>
   }
 
-  const center = {
-    lat: -41.296881158626114,
-    lng: 174.77414413921622,
-  }
-  const markerLocation = {
-    lat: -41.296881158626114,
-    lng: 174.77414413921622,
-  }
+  const center =
+    cafe.latitude && cafe.longitude
+      ? {
+          lat: cafe.latitude,
+          lng: cafe.longitude,
+        }
+      : defaultCenter
   return (
-    <LoadScript googleMapsApiKey={API_HOST}>
-      <GoogleMap mapContainerStyle={mapStyles} zoom={18} center={center}>
-        <Marker position={markerLocation} />
-      </GoogleMap>
-    </LoadScript>
+    <section className="border-accent-4 mx-auto my-8 w-3/4">
+      <LoadScriptNext
+        googleMapsApiKey={API_HOST}
+        loadingElement={<div>Loading...</div>}
+      >
+        <GoogleMap mapContainerStyle={mapStyles} zoom={18} center={center}>
+          <Marker position={center} />
+        </GoogleMap>
+      </LoadScriptNext>
+    </section>
   )
 }
