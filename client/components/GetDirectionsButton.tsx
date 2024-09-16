f2d367d4d813f489bf652173e46cd123f2fc0422
Shae Jacobs <shae.jacobs01@gmail.com>
2024-09-16 21:28:41 +1200

    updated so will use geolocation for accuracy

--- client/components/GetDirectionsButton.tsx
+++ client/components/GetDirectionsButton.tsx
@@ -7,11 +7,28 @@ interface CafeProp {
 
 export default function GetDirectionButton({ cafe }: CafeProp) {
   const openGoogleMapsForDirections = () => {
-    const latitude = cafe.latitude
-    const longitude = cafe.longitude
-    const url = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}&origin=current+location`
-    window.open(url, '_blank')
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userLatitude = position.coords.latitude
+          const userLongitude = position.coords.longitude
+          const cafelatitude = cafe.latitude
+          const cafelongitude = cafe.longitude
+          const url = `https://www.google.com/maps/dir/?api=1&destination=${cafelatitude},${cafelongitude}&origin=${userLatitude},${userLongitude}`
+          window.open(url, '_blank')
+        },
+        (error) => {
+          console.error('Error getting location: ', error)
+          alert(
+            'Unable to get your location. Please ensure location services are enabled.',
+          )
+        },
+      )
+    } else {
+      alert('Geolocation is not supported by your browser.')
+    }
   }
+
   return (
     <div className="flex gap-2">
       <Button onClick={openGoogleMapsForDirections}>Get Directions</Button>
