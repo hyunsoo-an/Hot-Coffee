01f47941f36adbef4f1c8a9e9f597dfe0fafff89
Hyunsoo An <hankjudolove@gmail.com>
2024-09-17 09:55:11 +1200

    changed location for css file

--- client/components/Loading.tsx
+++ client/components/Loading.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import styles from './Loading.module.css'
+import styles from '../styles/Loading.module.css'
 
 export default function LoadingSpinner() {
   const [isLoading, setIsLoading] = useState(true)
