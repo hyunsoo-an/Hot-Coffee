92fc61e8ad5c4146015dd17517f3c8c402430891
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-09-17 14:02:58 +1200

    Remove comments

--- client/components/Loading.tsx
+++ client/components/Loading.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-//import styles from '../styles/Loading.module.css'
 
 export default function LoadingSpinner() {
   const [isLoading, setIsLoading] = useState(true)
@@ -14,16 +13,6 @@ export default function LoadingSpinner() {
     return () => clearTimeout(timer)
   }, [navigate])
 
-  // return (
-  //   <div className={styles.loaderContainer}>
-  //     <h1 className={styles.title}>Filtering for Coffees</h1>
-  //     {isLoading && <div className={styles.loader}></div>}
-  //     {isLoading && (
-  //       <img src="/loading.png" alt="Loading" className={styles.image} />
-  //     )}
-  //     <p className={styles.subtitle}>...</p>
-  //   </div>
-  // )
   return (
     <div className="relative grid h-[70svh] grid-rows-[1fr_auto_1fr] items-center justify-center gap-dy bg-background text-center">
       <h1 className="self-end">Filtering for Coffees</h1>
