c9bafddbfdc6755031af2c730619aba1fb1ece1e
Hyunsoo An <hankjudolove@gmail.com>
2024-09-17 14:50:07 +1200

    another update for env

--- client/env.ts
+++ client/env.ts
@@ -1,3 +1,3 @@
-export const VITE_GOOGLE_API_KEY = import.meta.env.PROD
-  ? 'AIzaSyCvsmOWVIu6LFBL0Ft-bDK2p9-QZRqqY28'
-  : import.meta.env.VITE_GOOGLE_API_KEY
+export const VITE_GOOGLE_API_KEY =
+  import.meta.env.VITE_GOOGLE_API_KEY ||
+  'AIzaSyCvsmOWVIu6LFBL0Ft-bDK2p9-QZRqqY28'
