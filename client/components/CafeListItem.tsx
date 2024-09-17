fd261c7766a2fb3c2dfe4dd21def185f0401934d
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-09-17 16:35:11 +1200

    Make rating more obvious on cafe list

--- client/components/CafeListItem.tsx
+++ client/components/CafeListItem.tsx
@@ -1,3 +1,4 @@
+import { Star } from 'lucide-react'
 import { Cafe } from '../../models/cafes'
 import { Link } from 'react-router-dom'
 
@@ -6,13 +7,16 @@ export default function CafeListItem({ cafe }: { cafe: Cafe }) {
     <div className="rounded-lg bg-secondary p-4 text-secondary-foreground">
       <Link
         to={`/cafes/${cafe.id}`}
-        className="grid auto-rows-max grid-cols-[1fr_auto]"
+        className="grid auto-rows-max grid-cols-[1fr_auto] gap-x-dx"
       >
         <h2 className="font-semibold">{cafe.name}</h2>
-        <p className="text-sm text-muted-foreground">{`${cafe.suburb} ${cafe.distance}`}</p>
-        <p className="col-start-2 row-span-2 row-start-1 mr-3 flex items-center text-xl font-black">
-          {cafe.avgRating}
+        <p className="text-sm text-muted-foreground">
+          {cafe.distance ? `${cafe.distance} - ${cafe.suburb}` : cafe.suburb}
         </p>
+        <div className="col-start-2 row-span-2 row-start-1 flex items-center gap-2">
+          <p className="text-xl font-black">{cafe.avgRating}</p>{' '}
+          <Star className="size-4" />
+        </div>
       </Link>
     </div>
   )
