17e470545dc3c76a67da638a050a91f28d61e9fd
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-09-17 12:08:18 +1200

    Use tw classes for loader

--- client/components/Loading.tsx
+++ client/components/Loading.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import styles from '../styles/Loading.module.css'
+//import styles from '../styles/Loading.module.css'
 
 export default function LoadingSpinner() {
   const [isLoading, setIsLoading] = useState(true)
@@ -10,18 +10,34 @@ export default function LoadingSpinner() {
     const timer = setTimeout(() => {
       setIsLoading(false)
       navigate('/cafes')
-    }, 5000)
+    }, 500000)
     return () => clearTimeout(timer)
   }, [navigate])
 
+  // return (
+  //   <div className={styles.loaderContainer}>
+  //     <h1 className={styles.title}>Filtering for Coffees</h1>
+  //     {isLoading && <div className={styles.loader}></div>}
+  //     {isLoading && (
+  //       <img src="/loading.png" alt="Loading" className={styles.image} />
+  //     )}
+  //     <p className={styles.subtitle}>...</p>
+  //   </div>
+  // )
   return (
-    <div className={styles.loaderContainer}>
-      <h1 className={styles.title}>Filtering for Coffees</h1>
-      {isLoading && <div className={styles.loader}></div>}
+    <div className="relative grid h-[70svh] grid-rows-[1fr_auto_1fr] items-center justify-center gap-dy bg-background text-center">
+      <h1 className="self-end">Filtering for Coffees</h1>
       {isLoading && (
-        <img src="/loading.png" alt="Loading" className={styles.image} />
+        <>
+          <div className="col-span-full row-start-2 row-end-2 size-44 animate-spin place-self-center rounded-full border-2 border-t-2 border-muted border-t-accent"></div>
+          <img
+            src="/loading.png"
+            alt="Loading"
+            className="col-span-full row-start-2 row-end-2 size-40 place-self-center object-cover"
+          />
+        </>
       )}
-      <p className={styles.subtitle}>...</p>
+      <p className="self-start"></p>
     </div>
   )
 }
