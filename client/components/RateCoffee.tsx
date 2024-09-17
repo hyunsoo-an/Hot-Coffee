5291378687f0174ba46a8345778b081b9e0cfc09
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-09-18 09:41:45 +1200

    Push buttons donw

--- client/components/RateCoffee.tsx
+++ client/components/RateCoffee.tsx
@@ -3,7 +3,7 @@ import RateForm from './RateForm'
 export default function RateCoffee() {
   return (
     <section className="section">
-      <div className="content-wrapper pt-[15svh]">
+      <div className="content-wrapper pt-[20svh]">
         <RateForm />
       </div>
     </section>
